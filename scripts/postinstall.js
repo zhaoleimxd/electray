4defe347b4a6f26446e4a086fea47ac6fb30ab4b
zhaoleimxd <zhaoleimxd@163.com>
2023-01-13 09:59:46 +0800

    Fix: postinstall arch select.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,65 +1,93 @@
 const os = require('os');
 const fs = require('fs');
+const path = require('path');
 
-let osArch = os.arch();
-let pjArch = undefined;
-let rcArch = undefined;
-
-let pjPath = `package.json`;
-if (fs.existsSync(pjPath)) {
-    let pjStr = fs.readFileSync(pjPath, {
-        encoding: `utf8`
-    });
-    let pj = JSON.parse(pjStr);
-    if (pj[`config`] != undefined) {
-        pjArch = pj[`config`][`arch`]
+function findFileInParentDirs(fileName) {
+    let parentDir = path.resolve(`./`);
+    let lastDir = ``;
+    do {
+        lastDir = parentDir;
+        parentDir = path.resolve(`${parentDir}/../`);
+        let pjPath = path.resolve(`${parentDir}/${fileName}`);
+        if (fs.existsSync(pjPath)) {
+            return pjPath;
+        }
     }
+    while (lastDir != parentDir)
+
+    return undefined;
 }
 
-let rcPath = `.npmrc`;
-if (fs.existsSync(rcPath)) {
-    let rcStr = fs.readFileSync(rcPath, {
-        encoding: `utf8`
-    });
-    let pattern = /arch=(.+)\n/;
-    if (pattern.test(rcStr)) {
-        let ea = pattern.exec(rcStr);
-        rcArch = ea[1];
+function selectArch() {
+    let osArch = os.arch();
+    let pjArch = undefined;
+    let rcArch = undefined;
+
+    let pjPath = findFileInParentDirs(`package.json`);
+    if (fs.existsSync(pjPath)) {
+        let pjStr = fs.readFileSync(pjPath, {
+            encoding: `utf8`
+        });
+        let pj = JSON.parse(pjStr);
+        if (pj[`config`] != undefined) {
+            pjArch = pj[`config`][`arch`]
+        }
     }
-}
 
-let arch = undefined;
-if (pjArch != undefined) {
-    arch = pjArch;
-    console.log(`Use arch in package.json: ${arch}`);
-}
-else if (rcArch != undefined) {
-    arch = rcArch;
-    console.log(`Use arch in .npmrc: ${arch}`);
-}
-else {
-    arch = osArch;
-    console.log(`Use OS arch: ${arch}`);
-}
+    let rcPath = findFileInParentDirs(`.npmrc`);
+    if (fs.existsSync(rcPath)) {
+        let rcStr = fs.readFileSync(rcPath, {
+            encoding: `utf8`
+        });
+        let pattern = /arch=(.+)\n/;
+        if (pattern.test(rcStr)) {
+            let ea = pattern.exec(rcStr);
+            rcArch = ea[1];
+        }
+    }
 
-if (fs.existsSync(`lib`) == false) {
-    fs.mkdirSync(`lib`);
-}
-
-fs.copyFile(`src/electray/native.d.ts`, `lib/native.d.ts`, (error) => {
-    if (error == null) {
-        console.log(`lib/native.d.ts copied.`);
+    let arch = undefined;
+    if (pjArch != undefined) {
+        arch = pjArch;
+        console.log(`Use arch in ${pjPath}: ${arch}`);
+    }
+    else if (rcArch != undefined) {
+        arch = rcArch;
+        console.log(`Use arch in ${rcPath}: ${arch}`);
     }
     else {
-        console.log(`Error in copying file.`);
+        arch = osArch;
+        console.log(`Use OS arch: ${arch}`);
     }
-    fs.copyFile(`bin/native.${arch}.node`, `lib/native.node`, (error) => {
+
+    return arch;
+}
+
+function copyFiles() {
+    let arch = selectArch();
+    if (fs.existsSync(`lib`) == false) {
+        fs.mkdirSync(`lib`);
+    }
+
+    fs.copyFile(`src/electray/native.d.ts`, `lib/native.d.ts`, (error) => {
         if (error == null) {
-            console.log(`lib/native.node copied.`);
+            console.log(`lib/native.d.ts copied.`);
         }
         else {
             console.log(`Error in copying file.`);
         }
-        console.log(`Postinstall done.`);
+        fs.copyFile(`bin/native.${arch}.node`, `lib/native.node`, (error) => {
+            if (error == null) {
+                console.log(`lib/native.node copied.`);
+            }
+            else {
+                console.log(`Error in copying file.`);
+            }
+            console.log(`Postinstall done.`);
+        });
     });
-});
+}
+
+(function main() {
+    copyFiles();
+})();
